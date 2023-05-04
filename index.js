fabff09ffdb2bc22afe5912b11f13df907626536
shazidkarim <shazid747@gmail.com>
2023-05-04 18:24:17 +0600

    vercel added

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 const chef = require('./data/chef.json');
+const recipe = require('./data/recipe.json');
 app.use(cors());
 app.get('/', (req,res)=>{
     res.send('dragon is running');
 });
 app.get('/chef',(req,res)=>{
     res.send(chef); 
+})
+app.get('/recipe',(req,res)=>{
+    res.send(recipe); 
 })
 app.listen(port,()=>{
     console.log(`dragon is running on port:${port}`);
