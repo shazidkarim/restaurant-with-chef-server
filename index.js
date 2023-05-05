091942794ed5d4c2c5aba28237cd53d857af0393
shazidkarim <shazid747@gmail.com>
2023-05-05 17:53:38 +0600

    fixed data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const chef = require('./data/chef.json');
 const recipe = require('./data/recipe.json');
 app.use(cors());
 app.get('/', (req,res)=>{
-    res.send('dragon is running');
+    res.send('server is running');
 });
 app.get('/chef',(req,res)=>{
     res.send(chef); 
